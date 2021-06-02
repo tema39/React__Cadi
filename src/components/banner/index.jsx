08e42d00efe18207ee57dc0ed2388cff6188b28f
tema39 <artem.nomad@mail.ru>
2021-06-02 15:35:50 +0200

    add alert.js

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,22 +1,25 @@
-
 import "../banner/banner.scss";
 import React from "react";
 
-
 class Banner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.click = alert("Нет ссылки");
+  }
 
   render() {
-    return(
+    return (
       <div className="banner">
         <div className="banner__title-wrap">
-        <h1 className="banner__title">Hi, It’s Théophile Seyrig</h1>
-        <p className="banner__subtitle">Designer from Porto, Portugal</p>
-        <a className="banner__link-works" href="?">Latest works</a>
+          <h1 className="banner__title">Hi, It’s Théophile Seyrig</h1>
+          <p className="banner__subtitle">Designer from Porto, Portugal</p>
+          <a className="banner__link-works" onClick={this.click} href="?">
+            Latest works
+          </a>
         </div>
       </div>
-    )
+    );
   }
 }
 
 export default Banner;
-
