bb0401406d059c3907c2d38b6433e04a932c87ba
tema39 <artem.nomad@mail.ru>
2021-06-02 15:44:31 +0200

    fix href

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,48 +1,52 @@
-import React from 'react'
-import "../header/styles.scss"
-import mobileIcon from "../../images/NavButton.svg"
+import React from "react";
+import "../header/styles.scss";
+import mobileIcon from "../../images/NavButton.svg";
 
 class Header extends React.Component {
-constructor(props) {
-  super(props)
-  this.state = {
-    navigations: [
-      {
-        text: "Home",
-        link:"https://www.youtube.com/"
-      },
-      {
-        text:"About",
-        link: "https://www.youtube.com/"
-      },
-      {
-        text: "Skills",
-        link: "https://www.youtube.com/"
-      },
-      {
-        text: "Portfolio",
-        link: "https://www.youtube.com/"
-      },
-      {
-        text: "Contact",
-        link: "https://www.youtube.com/"
-      },
-    ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      navigations: [
+        {
+          text: "Home",
+          link: "https://www.google.ru/",
+        },
+        {
+          text: "About",
+          link: "https://www.google.ru/",
+        },
+        {
+          text: "Skills",
+          link: "https://www.google.ru/",
+        },
+        {
+          text: "Portfolio",
+          link: "https://www.google.ru/",
+        },
+        {
+          text: "Contact",
+          link: "https://www.google.ru/",
+        },
+      ],
+    };
   }
-}
   render() {
-    return(
+    return (
       <header className="header">
         <div className="header__logo-wrap">
-          <a className="header__logo" href="//#endregion">Portofolio</a>
+          <a className="header__logo" href="//#endregion">
+            Portofolio
+          </a>
         </div>
         <nav className="nav">
           <ul className="nav__list">
             {this.state.navigations.map((el) => (
               <li className="nav__list-item">
-              <a className="nav__list-item-link" href={el.link}>{el.text}</a>
-            </li>
-            )) }            
+                <a className="nav__list-item-link" href={el.link}>
+                  {el.text}
+                </a>
+              </li>
+            ))}
           </ul>
         </nav>
         <div className="mobile__btn-wrap">
@@ -50,9 +54,8 @@ constructor(props) {
             <img className="mobile__btn-icon" alt="icon" src={mobileIcon}></img>
           </button>
         </div>
-
       </header>
-    )
+    );
   }
 }
 export default Header;
